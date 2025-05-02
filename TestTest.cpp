4baefd925c2197db9d4bde1c007be2c0c0638b94
ArkandoLV <fallhost.ell@gmail.com>
2025-05-02 13:44:52 +0200

    Test add

--- TestTest.cpp
+++ TestTest.cpp
@@ -1,3 +1,48 @@
-//
-// Created by user on 5/2/2025.
-//
+#include <iostream>
+#include <deque>
+#include <string>
+
+using namespace std;
+struct Printjob {
+    int ID;
+    string doc_name;
+    bool priority;
+};
+
+deque<Printjob> printque;
+
+void add_job(const Printjob& job) {
+    if (job.priority) {
+        printque.push_front(job);
+        cout << "Priority job: " << job.doc_name << endl;
+    }
+    else {
+        printque.push_back(job);
+        cout << "Non priority job:" << job.doc_name << endl;
+    }
+}
+
+void next_job() {
+    if (!printque.empty()) {
+        Printjob job = printque.front();
+        printque.pop_front();
+        cout << "ID: " << job.ID << job.doc_name << endl;
+    }
+    else {
+        cout << "Queue is empty" << endl;
+    }
+}
+
+void print_content() {
+    if (printque.empty()) {
+        cout << "Queue is empty" << endl;
+    }
+    else {
+        for (const auto& job : printque) {
+            cout << "[ID:" << job.ID << "] "
+                 << job.doc_name << " "
+                 << (job.priority ? "Prio" : "No Prio")
+                 << endl;
+        }
+    }
+}
